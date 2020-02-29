efc7d5ff5574b5a47bf656f6b0d031de684f7e62
anarmawala <arshadn00@gmail.com>
2020-02-29 01:36:28 -0600

    Added toObj() to Article, added article array in Ticker

--- scraper/article.py
+++ scraper/article.py
@@ -3,15 +3,27 @@ import json
 
 class Article:
 
-    def __init__(self, url, headline, webpage_text="", ticker=""):
+    def __init__(self, url, headline, webpage_text="", ticker="", paragraphs=[]):
         self.webpage_text = webpage_text
         self.ticker = ticker
         self.url = url
         self.headline = headline
+        self.paragraphs = paragraphs
 
     def toJson(self):
         return json.dumps({
             "text": self.webpage_text,
             "ticker": self.ticker,
-            "url": self.url
+            "url": self.url,
+            "headline": self.headline,
+            "paragraphs": self.paragraphs
         })
+
+    def toObj(self):
+        return {
+            "text": self.webpage_text,
+            "ticker": self.ticker,
+            "url": self.url,
+            "headline": self.headline,
+            "paragraphs": self.paragraphs
+        }
