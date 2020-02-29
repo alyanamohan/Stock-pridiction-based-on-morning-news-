0b00808386d4139ab21c00ad545434347a674671
Mohamed Amn <mamn2@illinois.edu>
2020-02-29 01:02:53 -0600

    forbes URLs scrapesd

--- scraper/webscraper.py
+++ scraper/webscraper.py
@@ -1,3 +1,4 @@
+#DEPENDENCIES
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -7,3 +8,28 @@ from article import Article
 from ticker import Ticker
 
 browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+# all article urls
+article_urls = []
+
+def get_forbes():
+
+    browser.get(('https://www.forbes.com/money/'))
+
+    # Forbes Top 10 Articles
+    article_div = browser.find_element_by_xpath('//*[@id="row-2"]/div/div/div/div[1]/div/div')
+    articles = article_div.find_elements_by_tag_name('article')
+
+    forbes_article_urls = []
+
+    for article in articles:
+        stream_item_text = article.find_element_by_class_name('stream-item__text')
+        h2 = stream_item_text.find_element_by_tag_name('h2')
+        a = h2.find_element_by_tag_name('a')
+        href = a.get_attribute('href')
+        forbes_article_urls.append(href)
+
+    article_urls.extend(forbes_article_urls)
+
+get_forbes()
+print(article_urls)
