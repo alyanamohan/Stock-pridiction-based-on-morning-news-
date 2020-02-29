2bff3c3e73ec5c31eff7bbb2550ab23068fe968a
Brian Goldenberg <bggolden11@gmail.com>
2020-02-29 02:16:31 -0600

    fix for dependency issue

--- NLP/java-project/src/main/java/com/hackathon/NLP/Endpoints/GetStockInfo.java
+++ NLP/java-project/src/main/java/com/hackathon/NLP/Endpoints/GetStockInfo.java
@@ -7,15 +7,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import static spark.Spark.*;
 
 public class GetStockInfo {
+
     static ObjectMapper objectMapper = new ObjectMapper();
     public static void main(String[] args){
+        port(8080);
         StockResponse test = new StockResponse("test1","test2","test3",1,.5);
         try {
             get("/GetStockInfo", (req, res) -> {
                 return objectMapper.writeValueAsString(test);
             });
-            post("/PostStockInfo", (req, res) -> {
-                return objectMapper.writeValueAsString(test);
+
+            get("/PostStockInfo", (req, res) -> {
+                return "testing";
             });
         }
         catch(Exception e){
