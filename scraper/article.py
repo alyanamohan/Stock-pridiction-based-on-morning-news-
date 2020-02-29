04a6feeb9baa6afebc1b16c31cb2dc439dd916ff
anarmawala <arshadn00@gmail.com>
2020-02-29 01:18:16 -0600

    Added toJson() and headline to Article class

--- scraper/article.py
+++ scraper/article.py
@@ -1,6 +1,17 @@
+import json
+
+
 class Article:
 
-    def __init__(self, webpage_text, ticker, url):
+    def __init__(self, webpage_text, ticker, url, headline):
         self.webpage_text = webpage_text
         self.ticker = ticker
         self.url = url
+        self.headline = headline
+
+    def toJson(self):
+        return json.dumps({
+            "text": self.webpage_text,
+            "ticker": self.ticker,
+            "url": self.url
+        })
