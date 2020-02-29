7579d91240c9505dda37fdb7388a5b366e227f66
Mohamed Amn <mamn2@illinois.edu>
2020-02-29 10:57:01 -0600

    created list of top 1000 companies

--- scraper/webscraper.py
+++ scraper/webscraper.py
@@ -49,8 +49,77 @@ def populate_companies():
 
     browser.get('https://www.value.today/world-stock-news/world-top-1000-companies-list-market-cap-july-1st-2019')
 
+    company_names = {'S&P': True, 'Dow Jones': True}
+
     companies_table = browser.find_element_by_xpath('//*[@id="block-creative-responsive-theme-content"]/div/div/div[2]/table[1]/tbody')
-    
+    companies_list = companies_table.find_elements_by_tag_name('tr')
+    for company in companies_list:
+        company_name_point = company.find_elements_by_tag_name('td')
+        try:
+            company_name = company_name_point[1].find_element_by_tag_name('a').text
+            company_name = company_name.upper()
+            if 'INC' in company_name:
+                company_name = company_name[:company_name.find('INC') - 1]
+            if 'LLC' in company_name:
+                company_name = company_name[:company_name.find('LLC') - 1]
+            if '.,' in company_name:
+                company_name = company_name[:company_name.find('.,') - 2]
+            if 'LTD' in company_name:
+                company_name = company_name[:company_name.find('LTD') - 1]
+            if 'PLC' in company_name:
+                company_name = company_name[:company_name.find('PLC') - 1]
+            if 'P.L.C' in company_name:
+                company_name = company_name[:company_name.find('P.L.C') - 1]
+            if '(' in company_name:
+                company_name = company_name[:company_name.find('(') - 1]
+            if ' & ' in company_name:
+                company_name = company_name[:company_name.find(' & ')]
+            if 'LIMITED' in company_name:
+                company_name = company_name[:company_name.find('LIMITED') - 1]
+            if 'COMPAN' in company_name:
+                company_name = company_name[:company_name.find('COMPAN') - 1]
+            if 'LP' in company_name:
+                company_name = company_name[:company_name.find('LP') - 1]
+            if 'L.P' in company_name:
+                company_name = company_name[:company_name.find('L.P') - 1]
+            if 'CORP' in company_name:
+                company_name = company_name[:company_name.find('CORP') - 1]
+            if 'INTERNATIONAL' in company_name:
+                company_name = company_name[:company_name.find('INTERNATIONAL') - 1]
+            if 'HOLDING' in company_name:
+                company_name = company_name[:company_name.find('HOLDING') - 1]
+            if 'LABORATORIES' in company_name:
+                company_name = company_name[:company_name.find('LABORATORIES') - 1]
+            if ' AG' in company_name:
+                company_name = company_name[:company_name.find(' AG')]
+            if 'S.A' in company_name:
+                company_name = company_name[:company_name.find('S.A') - 1]
+            if ' SA' in company_name:
+                company_name = company_name[:company_name.find(' SA')]
+            if '.COM' in company_name:
+                company_name = company_name[:company_name.find('.COM')]
+            if 'N.V' in company_name:
+                company_name = company_name[:company_name.find('N.V') - 1]
+            if 'INDUSTRIES' in company_name:
+                company_name = company_name[:company_name.find('INDUSTRIES') - 1]
+            if 'TECHNOL' in company_name:
+                company_name = company_name[:company_name.find('TECHNOL') - 1]
+            if 'O.N' in company_name:
+                company_name = company_name[:company_name.find('O.N') - 1]
+            if 'GROUP' in company_name:
+                company_name = company_name[:company_name.find('GROUP') - 1]
+            if 'SOLUTION' in company_name:
+                company_name = company_name[:company_name.find('SOLUTION') - 1]
+
+            if len(company_name) is 0 or len(company_name) is 1:
+                continue
+        except:
+            continue
+
+        company_names[company_name] = True
+
+    print(len(company_names))
+
 
 
 def retrieve_forbes_article_data(article):
@@ -86,7 +155,9 @@ def retrieve_forbes_article_data(article):
 
 
 
-forbes_URLs = get_forbes_URLs()
-for article in forbes_URLs:
-    retrieve_forbes_article_data(article)
-    print(article.paragraphs)
+#forbes_URLs = get_forbes_URLs()
+#for article in forbes_URLs:
+#    retrieve_forbes_article_data(article)
+#    print(article.paragraphs)
+
+populate_companies()
